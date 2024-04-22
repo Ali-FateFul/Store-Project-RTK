6f2697d6f19154b7692092eb7b0b39a64e904c59
Ali FateFul <ali.fateful21@gmail.com>
2024-04-22 17:05:00 +0330

    layout managed

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,27 +1,27 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { PiShoppingCartSimpleBold } from "react-icons/pi";
-// import { useCart } from "../contexts/CartContext";
 import styles from "./Layout.module.css";
 
 function Layout({ children }) {
-  // const [state] = useCart();
-  return (
-    <>
-      <header className={styles.header}>
-        <Link to="/products">FateFul Shop</Link>
-        <Link to="/checkout">
-          <div>
-            <PiShoppingCartSimpleBold />
-            {/* {!!state.itemsCounter && <span>{state.itemsCounter}</span>} */}
-          </div>
-        </Link>
-      </header>
-      {children}
-      <footer className={styles.footer}>
-        <p>Developed by FateFul with ♥</p>
-      </footer>
-    </>
-  );
+	const state = useSelector((store) => store.cart);
+	return (
+		<>
+			<header className={styles.header}>
+				<Link to='/products'>FateFul Shop</Link>
+				<Link to='/checkout'>
+					<div>
+						<PiShoppingCartSimpleBold />
+						{!!state.itemsCounter && <span>{state.itemsCounter}</span>}
+					</div>
+				</Link>
+			</header>
+			{children}
+			<footer className={styles.footer}>
+				<p>Developed by FateFul with ♥</p>
+			</footer>
+		</>
+	);
 }
 
 export default Layout;
